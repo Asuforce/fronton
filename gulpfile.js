cfb3147b8dba726962be203a535a6fd5d13ef6f3
Shun Nishitsuji <asuforcegt@hotmail.co.jp>
2016-03-06 21:12:12 -0800

    browserSyncタスクを追加

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,21 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var htmlhint = require('gulp-htmlhint');
+var browserSync = require('browser-sync');
 
 gulp.task('sass', function() {
     gulp.src('app/src/sass/*.scss')
       .pipe(sass())
-      .pipe(gulp.dest('app/product/css'));
+      .pipe(gulp.dest('app/product/css'))
+      .pipe(browserSync.stream());
 });
 
 gulp.task('html', function(){
     gulp.src('app/src/**/*.html')
       .pipe(htmlhint())
       .pipe(htmlhint.reporter())
-      .pipe(gulp.dest('app/product/'));
+      .pipe(gulp.dest('app/product/'))
+      .pipe(browserSync.stream());
 });
 
 gulp.task('fail', function(){
@@ -23,6 +26,12 @@ gulp.task('fail', function(){
 });
 
 gulp.task('default', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./app/product/"
+        }
+    });
+
     gulp.watch('app/src/sass/*.scss', ['sass']);
     gulp.watch('app/src/**/*.html', ['html']);
 });
