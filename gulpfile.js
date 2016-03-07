c6fa0429b76f79d6f7b91c72080767394b1a5a56
Shun Nishitsuji <asuforcegt@hotmail.co.jp>
2016-03-06 20:44:51 -0800

    htmlhintタスクを追加

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var htmlhint = require('gulp-htmlhint');
 
 gulp.task('sass', function() {
     gulp.src('app/src/sass/*.scss')
@@ -7,6 +8,21 @@ gulp.task('sass', function() {
       .pipe(gulp.dest('app/product/css'));
 });
 
+gulp.task('html', function(){
+    gulp.src('app/src/**/*.html')
+      .pipe(htmlhint())
+      .pipe(htmlhint.reporter())
+      .pipe(gulp.dest('app/product/'));
+});
+
+gulp.task('fail', function(){
+    gulp.src('app/src/**/*.html')
+      .pipe(htmlhint())
+      .pipe(htmlhint.failReporter())
+      .pipe(gulp.dest('app/product/'));
+});
+
 gulp.task('default', function() {
-    gulp.watch("app/src/sass/*.scss", ["sass"]);
+    gulp.watch('app/src/sass/*.scss', ['sass']);
+    gulp.watch('app/src/**/*.html', ['html']);
 });
