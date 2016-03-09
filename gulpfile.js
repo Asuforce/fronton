ceece4b88e763e370547f9348294e279554216bc
Shun Nishitsuji <asuforcegt@hotmail.co.jp>
2016-03-08 19:11:28 -0800

    autoprefixを追加

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,12 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var htmlhint = require('gulp-htmlhint');
 var browserSync = require('browser-sync');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function() {
     gulp.src('app/src/sass/*.scss')
       .pipe(sass())
+      .pipe(autoprefixer())
       .pipe(gulp.dest('app/product/css'))
       .pipe(browserSync.stream());
 });
@@ -23,6 +25,10 @@ gulp.task('fail', function(){
       .pipe(htmlhint())
       .pipe(htmlhint.failReporter())
       .pipe(gulp.dest('app/product/'));
+});
+
+gulp.task('create', function() {
+    // TODO:productディレクトリ内を削除して再ビルドするタスク
 });
 
 gulp.task('default', function() {
